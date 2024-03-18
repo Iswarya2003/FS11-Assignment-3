0250373274486c201a1273624a74d847f2abd1ab
koushikakotha <105365912+koushikakotha@users.noreply.github.com>
2024-03-18 15:44:58 +0530

    Update Synchronization.java

--- Synchronization.java
+++ Synchronization.java
@@ -1,47 +1,39 @@
+//Synchronization
 class Counter {
     private int count = 0;
-
-
     public synchronized void increment() {
         count++;
     }
-
-
-    public synchronized int getCount() {
+    public int getCount() {
         return count;
     }
 }
 
-class MyThread extends Thread {
+class IncrementerThread extends Thread {
     private Counter counter;
+    private int times;
 
-    public MyThread(Counter counter) {
+    public IncrementerThread(Counter counter, int times) {
         this.counter = counter;
+        this.times = times;
     }
 
     public void run() {
-        for (int i = 0; i < 1000; i++) {
+        for (int i = 0; i < times; i++) {
             counter.increment();
         }
     }
 }
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws InterruptedException {
-        
         Counter counter = new Counter();
-
-        MyThread thread1 = new MyThread(counter);
-        MyThread thread2 = new MyThread(counter);
-
+        IncrementerThread thread1 = new IncrementerThread(counter, 1000);
+        IncrementerThread thread2 = new IncrementerThread(counter, 1000);
         thread1.start();
         thread2.start();
-
-    
         thread1.join();
         thread2.join();
-
-       
-        System.out.println("Final Counter Value: " + counter.getCount());
+        System.out.println("Final count: " + counter.getCount());
     }
 }
